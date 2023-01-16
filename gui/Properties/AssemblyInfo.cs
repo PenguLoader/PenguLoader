3ab40559eabf5fc62fb07ee2b0fe2a38f9550c9d
Nguyen Duy <wuuyi123@gmail.com>
2023-01-16 15:48:13 +0700

    Add GitHub company

--- gui/Properties/AssemblyInfo.cs
+++ gui/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("League Loader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("GitHub, Inc.")]
 [assembly: AssemblyProduct("League Loader")]
 [assembly: AssemblyCopyright("Copyright © Nomi. All rights reserved.")]
 [assembly: AssemblyTrademark("Application")]
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("24a12139-9146-4f36-b4a9-f9d50ca8fa6e")]
+[assembly: Guid("d4e30a84-9575-11ed-a1eb-0242ac120002")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
