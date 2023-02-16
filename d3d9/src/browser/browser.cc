fb1bbc0f9c98a92a1aec08366c4caf948d3fc6ad
Nguyen Duy <wuuyi123@gmail.com>
2023-02-16 10:51:01 +0700

    Implement IFEO without debugging

--- d3d9/src/browser/browser.cc
+++ d3d9/src/browser/browser.cc
@@ -273,6 +273,38 @@ static HWND WINAPI Hooked_CreateWindowExW(
     return hwnd;
 }
 
+static decltype(&CreateProcessW) Old_CreateProcessW = &CreateProcessW;
+static BOOL WINAPI Hooked_CreateProcessW(
+    _In_opt_ LPCWSTR lpApplicationName,
+    _Inout_opt_ LPWSTR lpCommandLine,
+    _In_opt_ LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    _In_opt_ LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    _In_ BOOL bInheritHandles,
+    _In_ DWORD dwCreationFlags,
+    _In_opt_ LPVOID lpEnvironment,
+    _In_opt_ LPCWSTR lpCurrentDirectory,
+    _In_ LPSTARTUPINFOW lpStartupInfo,
+    _Out_ LPPROCESS_INFORMATION lpProcessInformation)
+{
+    bool shouldHook = utils::strContain(lpCommandLine, L"LeagueClientUxRender.exe", false)
+        && utils::strContain(lpCommandLine, L"--type=renderer", false);
+
+    if (shouldHook)
+        dwCreationFlags |= CREATE_SUSPENDED;
+
+    BOOL ret = Old_CreateProcessW(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes,
+        bInheritHandles, dwCreationFlags, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation);
+
+    if (ret && shouldHook)
+    {
+        void InjectThisDll(HANDLE hProcess);
+        InjectThisDll(lpProcessInformation->hProcess);
+        ResumeThread(lpProcessInformation->hThread);
+    }
+
+    return ret;
+}
+
 void HookBrowserProcess()
 {
     // Open console window.
@@ -289,4 +321,6 @@ void HookBrowserProcess()
 
     // Hook CreateWindowExW().
     utils::hookFunc((void **)&Old_CreateWindowExW, Hooked_CreateWindowExW);
+    // Hook CreateProcessW().
+    utils::hookFunc((void **)&Old_CreateProcessW, Hooked_CreateProcessW);
 }
