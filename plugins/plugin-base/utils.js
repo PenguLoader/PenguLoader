8bc666adffcaaaa795ace74d688d9a02c6463635
Nguyen Duy <wuuyi123@gmail.com>
2023-02-26 08:26:40 +0700

    Add base plugins

---
+++ plugins/plugin-base/utils.js
@@ -0,0 +1,2 @@
+/* export constant */
+export const gretting = 'Hello, world!';
---
+++ plugins/theme-base/index.js
@@ -0,0 +1,6 @@
+/* import theme entry */
+import './theme.css';
+
+window.addEventListener('load', () => {
+  /* put your theme helpers here */
+});
