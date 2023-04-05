4a11e4641ae0461b086990381191bf5929ae6e00
Nguyen Duy <wuuyi123@gmail.com>
2023-04-06 00:13:17 +0700

    Add dialog threadings

--- core/src/browser/jsdialog.cc
+++ core/src/browser/jsdialog.cc
@@ -9,12 +9,16 @@ public:
     CustomJSDialogHandler() : CefRefCount(this)
     {
         cef_jsdialog_handler_t::on_jsdialog = on_jsdialog;
-        cef_jsdialog_handler_t::on_before_unload_dialog = on_before_unload_dialog;
-        cef_jsdialog_handler_t::on_reset_dialog_state = on_reset_dialog_state;
-        cef_jsdialog_handler_t::on_dialog_closed = on_dialog_closed;
     }
 
 private:
+    struct DialogData
+    {
+        wstring message;
+        cef_jsdialog_type_t type;
+        cef_jsdialog_callback_t *callback;
+    };
+
     static int CEF_CALLBACK on_jsdialog(struct _cef_jsdialog_handler_t* self,
         struct _cef_browser_t* browser,
         const cef_string_t* origin_url,
@@ -24,47 +28,36 @@ private:
         struct _cef_jsdialog_callback_t* callback,
         int* suppress_message)
     {
-        LPCWSTR message = L"";
-        if (message_text)
-            message = message_text->str;
+        if (dialog_type == JSDIALOGTYPE_ALERT || dialog_type == JSDIALOGTYPE_CONFIRM)
+        {
+            auto data = new DialogData();
+            data->type = dialog_type;
+            data->callback = callback;
+            data->message = message_text ? wstring(message_text->str, message_text->length) : L"";
 
-        if (dialog_type == JSDIALOGTYPE_ALERT)
-        {
-            MessageBoxW(rclient_window_, message, L"League of Legends Client", MB_ICONINFORMATION | MB_OK);
-            callback->cont(callback, 0, nullptr);
-            return true;
-        }
-        else if (dialog_type == JSDIALOGTYPE_CONFIRM)
-        {
-            int ret = MessageBoxW(rclient_window_, message, L"League of Legends Client", MB_ICONQUESTION | MB_YESNO);
-            callback->cont(callback, ret == IDYES, nullptr);
-            return true;
+            CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)dialog_thread, data, 0, NULL);
+            return 1;
         }
 
         return 0;
     }
 
-    static int CEF_CALLBACK on_before_unload_dialog(
-        struct _cef_jsdialog_handler_t* self,
-        struct _cef_browser_t* browser,
-        const cef_string_t* message_text,
-        int is_reload,
-        struct _cef_jsdialog_callback_t* callback)
+    static void CALLBACK dialog_thread(DialogData *data)
     {
-        return 0;
-    }
+        if (data->type == JSDIALOGTYPE_ALERT)
+        {
+            MessageBoxW(rclient_window_, data->message.c_str(),
+                L"League of Legends Client", MB_ICONINFORMATION | MB_OK);
+            data->callback->cont(data->callback, 0, nullptr);
+        }
+        else
+        {
+            int ret = MessageBoxW(rclient_window_, data->message.c_str(),
+                L"League of Legends Client", MB_ICONQUESTION | MB_YESNO);
+            data->callback->cont(data->callback, ret == IDYES, nullptr);
+        }
 
-    static void CEF_CALLBACK on_reset_dialog_state(
-        struct _cef_jsdialog_handler_t* self,
-        struct _cef_browser_t* browser)
-    {
-
-    }
-
-    static void CEF_CALLBACK on_dialog_closed(struct _cef_jsdialog_handler_t* self,
-        struct _cef_browser_t* browser)
-    {
-
+        delete data;
     }
 };
 
