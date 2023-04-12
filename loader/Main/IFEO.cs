2a3f64775afaaf88523946e7a4318c5fc0abbb2b
Soulmare <127675679+Yurichtofen@users.noreply.github.com>
2023-04-12 17:07:33 +0000

    Back to C# 7.3. I forgot about it. - @Yurichtofen

--- loader/Main/IFEO.cs
+++ loader/Main/IFEO.cs
@@ -10,31 +10,43 @@ namespace PenguLoader.Main
 
         public static string GetDebugger(string target)
         {
-            using var key = OpenIFEOKey();
-            using var image = key?.OpenSubKey(target);
-            return image?.GetValue(VALUE_NAME) as string;
+            using (var key = OpenIFEOKey())
+            {
+                using (var image = key?.OpenSubKey(target))
+                {
+                    return image?.GetValue(VALUE_NAME) as string;
+                }
+            }
         }
 
         public static bool SetDebugger(string target, string debugger)
         {
-            using var key = OpenIFEOKey(writable: true);
-            if (key == null) return false;
+            using (var key = OpenIFEOKey(writable: true))
+            {
+                if (key == null) return false;
 
-            using var image = key.CreateSubKey(target);
-            if (image == null) return false;
+                using (var image = key.CreateSubKey(target))
+                {
+                    if (image == null) return false;
 
-            image.SetValue(VALUE_NAME, debugger, RegistryValueKind.String);
-            return true;
+                    image.SetValue(VALUE_NAME, debugger, RegistryValueKind.String);
+                    return true;
+                }
+            }
         }
 
         public static void RemoveDebugger(string target)
         {
-            using var key = OpenIFEOKey(writable: true);
-            using var image = key?.OpenSubKey(target, true);
-            if (image == null) return;
+            using (var key = OpenIFEOKey(writable: true))
+            {
+                using (var image = key?.OpenSubKey(target, true))
+                {
+                    if (image == null) return;
 
-            image.DeleteValue(VALUE_NAME);
-            key.DeleteSubKey(target, false);
+                    image.DeleteValue(VALUE_NAME);
+                    key.DeleteSubKey(target, false);
+                }
+            }
         }
 
         private static RegistryKey OpenIFEOKey(bool writable = false) => Registry.LocalMachine.OpenSubKey(IFEO_PATH, writable);
