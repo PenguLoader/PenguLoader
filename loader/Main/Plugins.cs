c35f76c5f15aaf5fb458698464f8f26e77702350
Nguyen Duy <wuuyi123@gmail.com>
2023-04-26 10:09:27 +0700

    Fix plugin count filter

--- loader/Main/Plugins.cs
+++ loader/Main/Plugins.cs
@@ -12,13 +12,17 @@ namespace PenguLoader.Main
 
             // All top-level JS files.
             int topLevelFiles = Directory.GetFiles(pluginsDir, "*.js", SearchOption.TopDirectoryOnly)
-                .Count(path => !(Path.GetFileName(path).StartsWith(".") || Path.GetFileName(path).StartsWith("_")));
+                .Count(path => FilterName(Path.GetFileName(path)));
 
             // All sub-folders with index.js file.
             int subdirectoryFiles = Directory.GetDirectories(pluginsDir)
+                .Where(path => FilterName(Path.GetFileName(path)))
                 .Sum(subdirectory => Directory.GetFiles(subdirectory, "index.js", SearchOption.TopDirectoryOnly).Length);
 
             return topLevelFiles + subdirectoryFiles;
         }
+
+        // Ignore all file/dir names start with . or _
+        static bool FilterName(string name) => !(name.StartsWith(".") || name.StartsWith("_"));
     }
 }
