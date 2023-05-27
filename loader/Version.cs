c6abca76ff9500f813f26c049342a552e62799f4
Nguyen Duy <wuuyi123@gmail.com>
2023-05-27 11:32:44 +0700

    Beta v1.0.6

--- loader/Version.cs
+++ loader/Version.cs
@@ -5,10 +5,10 @@ namespace PenguLoader
     {
         public const string VERSION =
 #endif
-        "1.0.5"
+        "1.0.6"
 #if !__cplusplus
         ;
-        public const int BUILD_NUMBER = 1552;
+        public const int BUILD_NUMBER = 1600;
     }
 }
 #endif
