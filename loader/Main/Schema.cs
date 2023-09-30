6cd0a507309eff222c7c23642f7a2f5cc19dd7a9
Nguyen Duy <wuuyi123@gmail.com>
2023-09-30 20:31:45 +0700

    🔨 add symlink mode

---
+++ loader/Main/Schema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PenguLoader.Main
+{
+#pragma warning disable CS0649
+
+    static class Schema
+    {
+        public class RiotClientInstalls
+        {
+            public IDictionary<string, string> associated_client;
+            public string rc_default;
+            public string rc_live;
+        }
+    }
+
+#pragma warning restore CS0649
+}
