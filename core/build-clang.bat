6953047e3e7df0cc5bc6cafc24664fa4af4818bb
Nguyen Duy <wuuyi123@gmail.com>
2023-10-02 13:20:58 +0700

    🔨 fix + add delay load

--- core/build-clang.bat
+++ core/build-clang.bat
@@ -3,9 +3,11 @@
 mkdir obj\clang
 
 for /r .\src %%i in (*.cc) do (
-    %CC% -c -std=c++17 -O3 -DNDEBUG -DUNICODE -Wno-address-of-temporary -I ./src -I ./ -o ./obj/clang/%%~ni.obj "%%i"
+  clang++ -c -std=c++17 -O3 -DNDEBUG -DUNICODE -Wno-address-of-temporary ^
+    -I ./src -I ./ -o ./obj/clang/%%~ni.obj "%%i"
 )
 
-clang++ -shared -o ../bin/core.dll ./obj/clang/*.obj -Wl,/def:res/module.def libcef.lib -luser32
+clang++ -shared -o ../bin/core.dll ./obj/clang/*.obj ^
+  -Wl,/DEF:res/module.def,/DELAYLOAD:libcef.dll libcef.lib -luser32 -ldelayimp
 
 pause
