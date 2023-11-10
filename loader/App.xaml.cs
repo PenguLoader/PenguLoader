5aa7ecc9af842a19ed05df6b53b84e976d6c7a65
Dmitry <fiskdmitry@gmail.com>
2023-11-10 23:57:43 +0300

    russian translation

--- loader/App.xaml.cs
+++ loader/App.xaml.cs
@@ -46,6 +46,7 @@ namespace PenguLoader
             { "日本語", "ja-JP.xaml" },
             { "中文", "zh-CN.xaml" },
             { "Español", "es-ES.xaml" },
+            { "Русский", "ru-RU.xaml"}
         };
 
         public static void SetLanguage(string lang)
