f2d8514ba18b3c995402fc24ca3aada9ce4d5691
Nguyen Duy <wuuyi123@gmail.com>
2024-08-25 01:24:47 +0700

    preload: upgrade pkg, fix pengu version

--- plugins/src/preload/index.ts
+++ plugins/src/preload/index.ts
@@ -3,5 +3,7 @@ import './polyfills';
 import './super-potato';
 import './load-hooks';
 import './loader';
+import { version } from '../../package.json'
 
+window.Pengu.version = version
 Object.freeze(window.Pengu);
