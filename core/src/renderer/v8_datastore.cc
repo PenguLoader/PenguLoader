eb0e3a1b8e48cba1d1d90952a49c15a952c883d9
Nguyen Duy <wuuyi123@gmail.com>
2024-11-20 13:48:35 +0700

    update datastore with promise task

--- core/src/renderer/v8_datastore.cc
+++ core/src/renderer/v8_datastore.cc
@@ -1,6 +1,5 @@
 #include "pengu.h"
 #include "v8_wrapper.h"
-#include <thread>
 #include <mutex>
 
 static void transform_data(void *data, size_t length)
@@ -48,92 +47,42 @@ static void save_datastore(cef_string_utf8_t *json)
     }
 }
 
-struct DataStoreTask : CefRefCount<cef_task_t>
-{
-    V8Promise *promise;
-    cef_string_t *json;
-    cef_v8context_t *context;
-
-    DataStoreTask() : CefRefCount(this), json(nullptr)
-    {
-        context = cef_v8context_get_current_context();
-        context->base.add_ref(&context->base);
-
-        context->enter(context);
-        {
-            promise = V8Promise::create();
-            promise->addRef();
-        }
-        context->exit(context);
-
-        cef_bind_method(DataStoreTask, execute);
-    }
-
-    ~DataStoreTask()
-    {
-        if (json != nullptr)
-        {
-            cef_string_clear(json);
-            delete json;
-        }
-
-        context->base.release(&context->base);
-    }
-
-    void CEF_CALLBACK _execute()
-    {
-        context->enter(context);
-        {
-            V8Value *value = nullptr;
-            if (json != nullptr)
-                value = V8Value::string(json);
-
-            promise->resolve(value);
-            promise->release();
-        }
-        context->exit(context);
-    }
-};
-
 static V8Value *v8_load_datastore(V8Value *const args[], int argc)
 {
-    auto task = new DataStoreTask();
-    std::thread runner([task]
+    auto task = new V8Task();
+    return task->execute([task]
         {
-            task->json = new cef_string_t{};
-            load_datastore(task->json);
+            auto json = new cef_string_t{};
+            load_datastore(json);
 
-            cef_post_task(TID_RENDERER, task);
-        });
-
-    runner.detach();
-    return (V8Value *)task->promise;
+            task->resolve([json]
+                {
+                    auto value = V8Value::string(json);
+                    cef_string_clear(json);
+                    return value;
+                }
+            );
+        }
+    );
 }
 
 static V8Value *v8_save_datastore(V8Value *const args[], int argc)
 {
-    if (argc > 0 && args[0]->isString())
-    {
-        CefScopedStr json = args[0]->asString();
-        auto task = new DataStoreTask();
+    cef_string_userfree_t json = args[0]->asString();
 
-        auto utf8 = new cef_string_utf8_t{};
-        if (!json.empty())
-            cef_string_to_utf8(json.str, json.length, utf8);
+    auto task = new V8Task();
+    return task->execute([task, json]
+        {
+            auto utf8 = new cef_string_utf8_t{};
+            if (json && json->length)
+                cef_string_to_utf8(json->str, json->length, utf8);
 
-        std::thread runner([task, utf8]
-            {
-                save_datastore(utf8);
-                cef_string_utf8_clear(utf8);
+            save_datastore(utf8);
+            task->resolve();
 
-                cef_post_task(TID_RENDERER, task);
-            });
-
-        runner.detach();
-        return (V8Value *)task->promise;
-    }
-
-    return nullptr;
+            cef_string_utf8_clear(utf8);
+            cef_string_userfree_free(json);
+        });
 }
 
 V8HandlerFunctionEntry v8_DataStoreEntries[]
