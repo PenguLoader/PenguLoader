97e930d6a44c7462a7d782182a7693623cf54b6e
Nguyen Duy <wuuyi123@gmail.com>
2024-12-01 20:35:31 +0700

    add pengu version to console

--- plugins/src/preload/index.ts
+++ plugins/src/preload/index.ts
@@ -2,7 +2,9 @@ import './api';
 import './super-potato';
 import './load-hooks';
 import './loader';
-import { version } from '../../package.json'
+import { version } from '../../package.json';
 
-window.Pengu.version = version
+window.Pengu.version = version;
 Object.freeze(window.Pengu);
+
+console.info(`%c Pengu Loader v${version} `, 'background: #000; color: #fff;');
