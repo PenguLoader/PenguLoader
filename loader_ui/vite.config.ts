c9d423db9f73e056a3036f361c2f3111044a61bc
Nguyen Duy <wuuyi123@gmail.com>
2025-12-06 18:54:21 +0700

    chore: update project config

--- loader_ui/vite.config.ts
+++ loader_ui/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
-import UnocssPlugin from '@unocss/vite';
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import devtools from 'solid-devtools/vite'
+import UnocssPlugin from '@unocss/vite'
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
@@ -19,4 +20,11 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
-});
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@lib': path.resolve(__dirname, 'src/lib'),
+    },
+  },
+})
