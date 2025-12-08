485e432fbac2a4c67aae4358fa3952593e25d7e1
Nguyen Duy <wuuyi123@gmail.com>
2025-12-08 17:54:35 +0700

    chore: remove https, set fixed asset name

--- loader_ui/vite.config.ts
+++ loader_ui/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import devtools from 'solid-devtools/vite'
 import UnocssPlugin from '@unocss/vite'
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   plugins: [
@@ -11,7 +10,6 @@ export default defineConfig({
     UnocssPlugin({
       // your config or in uno.config.ts
     }),
-    // mkcert(),
   ],
   server: {
     port: 3000,
@@ -19,6 +17,13 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      }
+    }
   },
   resolve: {
     alias: {
