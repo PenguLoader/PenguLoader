a6469c20b0151ced1ab4c4d6f183d1cf620b9b7d
Nguyen Duy <wuuyi123@gmail.com>
2026-01-28 19:08:23 +0700

    feat: add core module manipulation

---
+++ loader/Core/IFEO.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace Pengu.Loader.Core
+{
+    static class IFEO
+    {
+        static string IFEO_PATH => @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options";
+        static string VALUE_NAME => "Debugger";
+
+        public static string? GetDebugger(string target)
+        {
+            using (var key = Registry.LocalMachine.OpenSubKey(IFEO_PATH))
+            {
+                if (key == null)
+                    return string.Empty;
+
+                using (var image = key.OpenSubKey(target))
+                {
+                    if (image == null)
+                        return string.Empty;
+
+                    return image.GetValue(VALUE_NAME) as string;
+                }
+            }
+        }
+
+        public static void SetDebugger(string target, string debugger)
+        {
+            debugger = debugger.Replace("\"", "\\\"");
+            RunRegCommand($"add \"HKLM\\{IFEO_PATH}\\{target}\" /v \"{VALUE_NAME}\" /t REG_SZ /d \"{debugger}\" /f");
+        }
+
+        public static void RemoveDebugger(string target)
+        {
+            RunRegCommand($"delete \"HKLM\\{IFEO_PATH}\\{target}\" /f");
+        }
+
+        /// <summary>
+        /// Executes a reg command with elevated privileges.
+        /// </summary>
+        /// <remarks>
+        /// Should use external reg.exe tool to modify registry with admin rights.
+        /// Modifying registry keys directly via RegistryKey class may trigger AV.
+        /// </remarks>
+        /// <exception cref="InvalidOperationException">Thrown if the command fails for any reason other than access denied.</exception>
+        /// <exception cref="UnauthorizedAccessException">Thrown if access is denied or the operation is canceled by the user.</exception>
+        public static void RunRegCommand(string args)
+        {
+            try
+            {
+                using (var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = $"/C reg {args}",
+                        UseShellExecute = true,
+                        CreateNoWindow = true,
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        Verb = "runas",
+                    }
+                })
+                {
+                    process.Start();
+                    process.WaitForExit();
+
+                    int exitCode = process.ExitCode;
+
+                    if (exitCode != 0)
+                    {
+                        if (exitCode == 5)
+                            throw new UnauthorizedAccessException("Access denied when trying to run reg command.");
+
+                        throw new InvalidOperationException($"Reg command failed with exit code: {exitCode}");
+                    }
+                }
+            }
+            catch (Win32Exception ex) when (ex.NativeErrorCode == 1223)
+            {
+                // The operation was canceled by the user.
+                throw new UnauthorizedAccessException("The operation was canceled by the user.", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Failed to execute reg command.", ex);
+            }
+        }
+    }
+}
---
+++ loader/Core/Module.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace Pengu.Loader.Core
+{
+    internal class Module
+    {
+        private static string ModuleName => "core.dll";
+        private static string TargetName => "LeagueClientUx.exe";
+        private static string ModulePath => Path.Combine(Config.BaseDir, ModuleName);
+        private static string DebuggerValue => $"rundll32 \"{ModulePath}\", #6000 ";
+
+        public static bool IsFound()
+        { 
+            return File.Exists(ModulePath);
+        }
+
+        public static bool IsLoaded()
+        {
+            var path = ModulePath;
+            if (!File.Exists(path)) return false;
+            try
+            {
+                using var _ = new FileStream(path, FileMode.Open,
+                    FileAccess.ReadWrite, FileShare.None);
+            }
+            catch
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsActive()
+        {
+            var param = IFEO.GetDebugger(TargetName);
+            return DebuggerValue.Equals(param, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <exception cref="Exception"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        public static bool SetActive(bool active)
+        {
+            if (IsActive() == active)
+                return true;
+
+            if (active)
+            {
+                IFEO.SetDebugger(TargetName, DebuggerValue);
+            }
+            else
+            {
+                IFEO.RemoveDebugger(TargetName);
+            }
+
+            return IsActive() == active;
+        }
+    }
+}
