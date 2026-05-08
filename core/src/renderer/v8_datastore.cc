7698f940a81bdc1d6c506855a78db70e026533b9
Ku-Tadao <29869255+Ku-Tadao@users.noreply.github.com>
2026-05-08 17:01:57 +0200

    Queue DataStore commits asynchronously

--- core/src/renderer/v8_datastore.cc
+++ core/src/renderer/v8_datastore.cc
@@ -1,6 +1,16 @@
 #include "pengu.h"
 #include "v8_wrapper.h"
 
+#include <atomic>
+#include <mutex>
+#include <string>
+
+namespace
+{
+    std::mutex g_datastore_write_mutex;
+    std::atomic<uint64_t> g_datastore_save_sequence{ 0 };
+}
+
 static void transform_data(void *data, size_t length)
 {
     static const char key[] = "A5dgY6lz9fpG9kGNiH1mZ";
@@ -34,12 +44,12 @@ static void load_datastore(cef_string_t *json)
     }
 }
 
-static void save_datastore(cef_string_utf8_t *json)
+static void save_datastore(std::string json)
 {
     auto path = config::datastore_path();
 
-    transform_data(json->str, json->length);
-    file::write_file(path, json->str, json->length);
+    transform_data(json.data(), json.size());
+    file::write_file(path, json.data(), json.size());
 }
 
 static V8Value *v8_load_datastore(V8Value *const args[], int argc)
@@ -63,7 +73,16 @@ static V8Value *v8_save_datastore(V8Value *const args[], int argc)
         {
             cef_string_utf8_t utf8{};
             cef_string_to_utf8(json.str, json.length, &utf8);
-            save_datastore(&utf8);
+
+            std::string payload(utf8.str, utf8.length);
+            auto sequence = g_datastore_save_sequence.fetch_add(1) + 1;
+            v8_async::enqueue([sequence, payload = std::move(payload)]() mutable {
+                std::lock_guard<std::mutex> lock(g_datastore_write_mutex);
+                if (sequence != g_datastore_save_sequence.load())
+                    return;
+
+                save_datastore(std::move(payload));
+            });
 
             cef_string_utf8_clear(&utf8);
         }
