87987aece0582e8af4badf5f258b06f3e276781e
Ku-Tadao <29869255+Ku-Tadao@users.noreply.github.com>
2026-05-08 17:11:35 +0200

    Coalesce async DataStore saves

--- core/src/renderer/v8_datastore.cc
+++ core/src/renderer/v8_datastore.cc
@@ -1,14 +1,16 @@
 #include "pengu.h"
 #include "v8_wrapper.h"
 
-#include <atomic>
 #include <mutex>
+#include <optional>
 #include <string>
 
 namespace
 {
+    std::mutex g_datastore_pending_mutex;
     std::mutex g_datastore_write_mutex;
-    std::atomic<uint64_t> g_datastore_save_sequence{ 0 };
+    std::optional<std::string> g_datastore_pending_save;
+    bool g_datastore_save_queued = false;
 }
 
 static void transform_data(void *data, size_t length)
@@ -44,12 +46,35 @@ static void load_datastore(cef_string_t *json)
     }
 }
 
-static void save_datastore(std::string json)
+static void save_datastore(std::string &json)
 {
     auto path = config::datastore_path();
 
     transform_data(json.data(), json.size());
     file::write_file(path, json.data(), json.size());
+}
+
+static void drain_datastore_saves()
+{
+    for (;;)
+    {
+        std::optional<std::string> payload;
+
+        {
+            std::lock_guard<std::mutex> lock(g_datastore_pending_mutex);
+            if (!g_datastore_pending_save.has_value())
+            {
+                g_datastore_save_queued = false;
+                return;
+            }
+
+            payload.emplace(std::move(g_datastore_pending_save.value()));
+            g_datastore_pending_save.reset();
+        }
+
+        std::lock_guard<std::mutex> lock(g_datastore_write_mutex);
+        save_datastore(payload.value());
+    }
 }
 
 static V8Value *v8_load_datastore(V8Value *const args[], int argc)
@@ -74,15 +99,20 @@ static V8Value *v8_save_datastore(V8Value *const args[], int argc)
             cef_string_utf8_t utf8{};
             cef_string_to_utf8(json.str, json.length, &utf8);
 
-            std::string payload(utf8.str, utf8.length);
-            auto sequence = g_datastore_save_sequence.fetch_add(1) + 1;
-            v8_async::enqueue([sequence, payload = std::move(payload)]() mutable {
-                std::lock_guard<std::mutex> lock(g_datastore_write_mutex);
-                if (sequence != g_datastore_save_sequence.load())
-                    return;
+            bool should_queue = false;
+            {
+                std::lock_guard<std::mutex> lock(g_datastore_pending_mutex);
+                g_datastore_pending_save.emplace(utf8.str, utf8.length);
 
-                save_datastore(std::move(payload));
-            });
+                if (!g_datastore_save_queued)
+                {
+                    g_datastore_save_queued = true;
+                    should_queue = true;
+                }
+            }
+
+            if (should_queue)
+                v8_async::enqueue(drain_datastore_saves);
 
             cef_string_utf8_clear(&utf8);
         }
